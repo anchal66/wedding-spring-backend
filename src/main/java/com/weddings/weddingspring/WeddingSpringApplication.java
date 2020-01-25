bfcd07b447333f4851ada4131eee30a53f0879c9
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2020-01-25 12:05:53 +0530

    Initial commit

---
+++ src/main/java/com/weddings/weddingspring/WeddingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.weddings.weddingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weddings/weddingspring/WeddingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weddings.weddingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
